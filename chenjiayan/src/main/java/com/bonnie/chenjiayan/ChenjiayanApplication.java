89be3f089c55a5be3f67855b26df2bfa73883a2c
zhangliqian <13124847347@163.com>
2023-07-15 20:25:19 +0800

    折腾了一天了，总是搞不定之前的项目建立子项目，总是加载不到配置文件，只要从其他项目建立好子项目后，再把原来的功能加进去

---
+++ chenjiayan/src/main/java/com/bonnie/chenjiayan/ChenjiayanApplication.java
@@ -0,0 +1,13 @@
+package com.bonnie.chenjiayan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChenjiayanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChenjiayanApplication.class, args);
+    }
+
+}
